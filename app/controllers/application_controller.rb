62c451534244516ecca7fa148dfb611cce42ab2f
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-10-04 14:03:41 -0500

    first attempt to fix x-frame issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 	after_filter :ahoy_track
+	after_filter :set_header_for_iframe
+
+	private
+	def set_header_for_iframe
+		response.headers.delete "X-Frame-Options" 
+	end
 
 	protected
-
 	def ahoy_track
 		ahoy.track_visit
 	end
